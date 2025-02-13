b84afe170b92d7976f01b78fb2f3e8f27908938d
bulat1337 <bulatmot@gmail.com>
2025-02-13 22:51:14 +0300

    avoid recursion

--- OnCPU/include/bitonic_sort.h
+++ OnCPU/include/bitonic_sort.h
@@ -3,34 +3,43 @@
 #include <vector>
 #include <algorithm>
 #include <iostream>
+#include <stack>
 
 namespace bitonic
 {
 	template <typename RandomIt>
 	void Sort(RandomIt begin, RandomIt end)
 	{
-		size_t data_size = std::distance(begin, end);
-		// std::sort(begin, end);
+		using range_t = std::pair<RandomIt, RandomIt>;
+		std::stack<range_t> stk;
+		stk.push({begin, end});
 
-		if (data_size <= 1) return;
-
-
-		for (size_t i = 0; i < data_size / 2; ++i)
+		while (!stk.empty())
 		{
-			auto lhs_iter = std::next(begin, i);
-			auto rhs_iter = std::next(begin, i + data_size / 2);
+			auto [cur_begin, cur_end] = stk.top();
+			stk.pop();
 
-			if (*lhs_iter > *rhs_iter)
+			size_t data_size = std::distance(cur_begin, cur_end);
+
+			if (data_size <= 1) continue;
+
+			auto mid = std::next(cur_begin, data_size / 2);
+
+			auto lhs_iter = cur_begin;
+			auto rhs_iter = mid;
+
+			for (;rhs_iter != cur_end; ++lhs_iter, ++rhs_iter)
 			{
-				std::cout << "swapping " << *lhs_iter << " and " << *rhs_iter << '\n';
-				std::iter_swap(lhs_iter, rhs_iter);
+				if (*lhs_iter > *rhs_iter)
+				{
+					std::cout << "swapping " << *lhs_iter << " and " << *rhs_iter << '\n';
+					std::iter_swap(lhs_iter, rhs_iter);
+				}
 			}
+
+			stk.push({cur_begin, mid});
+			stk.push({mid, cur_end});
 		}
-
-		auto mid = std::next(begin, data_size / 2);
-
-		Sort(begin, mid);
-		Sort(mid, end);
 	}
 
 
